7be7246f7b30bd17b88d0a16e0b1656ad78a12a4
Kim Ji Eun <{>
2024-11-16 23:30:28 +0900

    ex2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import List from "./components/List";
+import { useMemo, useState } from 'react';
+import List from './components/List';
 
 // TODO: 콘솔창을 확인해보고 input 타이핑할 때 마다
 //       List 컴포넌트가 불필요하게 리렌더링되지 않게 useMemo 사용해서 해결해 보세요.
 
 const App = () => {
-  const [input, setInput] = useState("");
-  const [items, setItems] = useState(["Item 1", "Item 2", "Item 3"]);
+  const [input, setInput] = useState('');
+  const [items, setItems] = useState(['Item 1', 'Item 2', 'Item 3']);
 
   const handleInputChange = (event) => {
     setInput(event.target.value);
@@ -14,12 +14,12 @@ const App = () => {
 
   const addItem = () => {
     setItems((prevItems) => [...prevItems, input]);
-    setInput("");
+    setInput('');
   };
 
-  const filteredItems = items.filter((item) =>
-    item.toLocaleLowerCase().includes("item"),
-  );
+  const filteredItems = useMemo(() => {
+    return items.filter((item) => item.toLocaleLowerCase().includes('item'));
+  }, []);
 
   return (
     <div>
