d0565154728804d9149e00cb29202bda2c7e3bbd
Kim Ji Eun <{>
2024-11-16 21:26:59 +0900

    ex1

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,6 +1,9 @@
+import { memo } from 'react';
+
 // TODO: input 타이핑할 때 리스트 컴포넌트가 불필요하게 렌더링되지 않게 해보세요.
-const List = ({ items }) => {
-  console.log("List component rendered");
+// eslint-disable-next-line react/display-name
+const List = memo(({ items }) => {
+  console.log('List component rendered');
   return (
     <ul>
       {items.map((item, index) => (
@@ -8,6 +11,6 @@ const List = ({ items }) => {
       ))}
     </ul>
   );
-};
+});
 
 export default List;
